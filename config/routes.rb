e29b448a4a7f5c745f0055e12ea9b772f63e027f
Channely <qijie29896@gmail.com>
2013-12-02 18:33:35 +0800

    the code only :VIEW <=> CONTROLLER <=> ROUTES

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 ActivityServer::Application.routes.draw do
+  resources :posts
   root 'navigate#home'
-
   get "navigate/home"
+
+
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -56,4 +59,3 @@ ActivityServer::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
